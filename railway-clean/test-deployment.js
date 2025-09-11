8f9e5dbefc3599cc9f0410cb293e49c63a327ace
Warusi2023 <warusi2023@gmail.com>
2025-09-11 19:43:24 +0300

    Trigger Railway deployment with test file
    This test file will trigger Railway to:
    1. Detect the railway-clean directory
    2. Build the Express server
    3. Deploy to Railway
    4. Provide the actual Railway URL
    
    After successful deployment, this test file will be removed.

---
+++ railway-clean/test-deployment.js
@@ -0,0 +1,4 @@
+// Test file to trigger Railway deployment
+console.log('Railway deployment test file created');
+console.log('Timestamp:', new Date().toISOString());
+console.log('This file will be deleted after successful deployment');
