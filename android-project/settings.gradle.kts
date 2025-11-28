03a9f2e429936c71f7984b70d315814229e15532
Warusi2023 <warusi2023@gmail.com>
2025-11-28 10:29:47 +0300

    fix: Update settings.gradle.kts to use FAIL_ON_PROJECT_REPOS and centralized repositories

--- android-project/settings.gradle.kts
+++ android-project/settings.gradle.kts
@@ -3,22 +3,20 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") } // For MPAndroidChart
-        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
-        maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev") }
-        ivy { url = uri("https://nodejs.org/dist") } // For Kotlin JS Node.js support
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
 rootProject.name = "SmartFarm"
+
 include(":androidApp")
 include(":shared")
-include(":web")
